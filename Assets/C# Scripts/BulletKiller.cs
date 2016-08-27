df603c637cd9b8a6f05d1a65171dbeaeb3be2e45
MegaTuks <a_marcel93@hotmail.com>
2016-08-27 15:22:14 -0500

    obstacle functionality

---
+++ Assets/C# Scripts/BulletKiller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletKiller : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("algo choco?");
+        if (col.gameObject.tag == "Bullet")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/C# Scripts/ObstacleController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObstacleController : MonoBehaviour
+{
+    
+    public Vector3 mMovementSpeed;
+    public int mObstacleType;
+    Vector2 mTargetPosition;
+    public GameObject mTarget;
+    public Sprite mOsbstacleImage;
+	// Use this for initialization
+	void Start ()
+    {
+ 
+	}
+
+    public void setObstacle(int type, Vector3 speed)
+    {
+        mObstacleType = type;
+        mMovementSpeed = speed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.position = gameObject.transform.position + mMovementSpeed;
+    }
+}
